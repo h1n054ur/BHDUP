f4581b5cbfc6ce97de29d3b729262d3a50f8b0e8
fachizel <78293294+fachizel@users.noreply.github.com>
2021-02-01 01:53:33 +1100

    Update bhdup.py

--- bhdup.py
+++ bhdup.py
@@ -83,9 +83,9 @@ for t in tqdm(torrents):
     os.system("mediainfo " + t + "/" + video[0] + " > ./mediainfo.txt")
     print(Fore.YELLOW + "Mediainfo Generated")
     time.sleep(2)
-    screen_shots(t, video[0])
 
     # UPLOAD THUMBNAILS AND SAVE BBCODE TO VARIABLE
+    screen_shots(t, video[0])
     print(Fore.CYAN + "Starting Upload to imgbox")
     os.system("imgbox *.png -w 350 > tmp")
     os.system(r"grep -R 'Webpage\|Thumbnail' tmp | awk -F ' ' '{print $2}' | sed '1~2s/\(.*\)/]\[img]\1\[\/img]\[\/URL]/g ; N;s/\(.*\)\n\(.*\)/\2\1/ ; s/^/\[URL=/' | tr -d '[:space:]' > ./" + t + ".files/bbcode.txt")
